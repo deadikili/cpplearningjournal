3602ae559ec22530b5bd716f767e0c5069d11fbb
jayshozie <emirbyildirim@gmail.com>
2022-07-31 13:45:36 +0300

    Section 3/3bCOMMIT

---
+++ Section 3/3bPlayground4Pointers/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int x = 4;
+    int *pX = &x;
+    cout << pX << endl;
+
+
+    system("PAUSE");
+    return 0;
+}
